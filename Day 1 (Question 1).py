d7bc0bb27e0f068b77291f7d16588d10f9270824
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-07 11:46:35 +0100

    Update Day 1 (Question 1).py

--- Day 1 (Question 1).py
+++ Day 1 (Question 1).py
@@ -1,7 +1,7 @@
 # Note: pandas and numpy are already imported as pd and np
 # The following tables are loaded as pandas DataFrames with the same names: dim_groups
 # Please print your final result or dataframe
-
+import pandas as pd
 #convert the data type on the 'created_date' column
 dim_groups['created_date']=pd.to_datetime(dim_groups['created_date'] )
 
@@ -12,10 +12,4 @@ filtered_df = dim_groups[(dim_groups['created_date'] >= start_date) & (dim_group
 
 #print the maximum participant_count
 print(filtered_df["participant_count"].max())
-# def dateconversion(x):
-#   dim_groups[x]=pd.to_datetime(dim_groups[x] )
-#   start
-#   largestgroup=pd.dim_groups[x].max()
-#   print(largestgroup)
 
-# dim_groups["created_date"].apply(dateconversion)
