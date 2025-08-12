995be8f69c15167d677d884703afd43b3cf71274
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-12 20:46:04 +0100

    Create Day 9 (Question 1).py

---
+++ Day 9 (Question 1).py
@@ -0,0 +1,6 @@
+# Note: pandas and numpy are already imported as pd and np
+# The following tables are loaded as pandas DataFrames with the same names: stories_data
+# Please print your final result or dataframe
+
+stories_data['story_date']=pd.to_datetime(stories_data['story_date'],errors='coerce')
+print(stories_data)
