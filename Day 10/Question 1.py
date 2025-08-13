fdc6c216a9e1703bb8f10012f46e34adf376f4ff
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-13 13:07:58 +0100

    Update Question 1.py

--- Day 10/Question 1.py
+++ Day 10/Question 1.py
@@ -4,6 +4,10 @@
 # The following tables are loaded as pandas DataFrames with the same names: app_ratings
 # Please print your final result or dataframe
 
+
+app_ratings=pd.read_csv('Day 10/app_ratings.csv')
+
+
 app_ratings['rating']=app_ratings['rating'].str.replace(',','.').str.strip()
 app_ratings['rating']=pd.to_numeric(app_ratings['rating'],errors='coerce')
 print(app_ratings)
