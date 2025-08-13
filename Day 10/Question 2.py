39f2ce1aa7279154dde5003176d42dd7b0a72171
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-13 13:00:51 +0100

    Create Question 2.py

---
+++ Day 10/Question 2.py
@@ -0,0 +1,11 @@
+#Using the cleaned dataset, display the first and last five entries to get an overview of the app ratings across different categories.
+
+# Note: pandas and numpy are already imported as pd and np
+# The following tables are loaded as pandas DataFrames with the same names: app_ratings
+# Please print your final result or dataframe
+
+app_ratings['rating']=app_ratings['rating'].str.replace(',','.').str.strip()
+app_ratings['rating']=pd.to_numeric(app_ratings['rating'],errors='coerce')
+overview=pd.concat([(app_ratings.head(5)),(app_ratings.tail(5))], axis=0)
+
+print(overview.to_string(index=False))
