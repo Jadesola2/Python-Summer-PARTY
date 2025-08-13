b0cd7664f7c1cc763a41c8a5c5317376add320f4
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-13 10:16:22 +0100

    Create Day 11 (Question 3).py

---
+++ Day 11 (Question 3).py
@@ -0,0 +1,14 @@
+# Note: pandas and numpy are already imported as pd and np
+# The following tables are loaded as pandas DataFrames with the same names: fct_transactions, dim_risk_flags
+# Please print your final result or dataframe
+
+fct_transactions['transaction_date']=pd.to_datetime(fct_transactions['transaction_date'])
+start_date='2024-12-01'
+end_date='2024-12-31'
+filtered_fct_transactions=fct_transactions[(fct_transactions['transaction_date']>=start_date)& (fct_transactions['transaction_date']<=end_date)]
+filtered_fct_transactions['day_of_the_week']=filtered_fct_transactions['transaction_date'].dt.day_name()
+merged_df=pd.merge(filtered_fct_transactions,dim_risk_flags,how='left',on='transaction_id')
+high_risk = merged_df[merged_df['risk_level'] == 'High']
+counts = high_risk.groupby('day_of_the_week').size()
+most_common_day = counts.idxmax()
+print(most_common_day, counts[most_common_day])
