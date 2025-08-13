fc0f1b08981cb1c1b38cf619ffcb635c8a16523f
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-13 13:43:02 +0100

    Delete Day 10 (Question 1).py

--- Day 10 (Question 1).py
+++
@@ -1,7 +0,0 @@
-# Note: pandas and numpy are already imported as pd and np
-# The following tables are loaded as pandas DataFrames with the same names: app_ratings
-# Please print your final result or dataframe
-
-app_ratings['rating']=app_ratings['rating'].str.replace(',','.').str.strip()
-app_ratings['rating']=pd.to_numeric(app_ratings['rating'],errors='coerce')
-print(app_ratings)
