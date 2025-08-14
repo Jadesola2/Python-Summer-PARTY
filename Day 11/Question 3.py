933883a8d8bc9ba5fd93adc58bd89129ab1a44c1
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-14 20:09:18 +0100

    Update Question 3.py

--- Day 11/Question 3.py
+++ Day 11/Question 3.py
@@ -4,13 +4,28 @@
 
 # Please print your final result or dataframe
 
+#convert the 'transaction_date' column to a datetime format
 fct_transactions['transaction_date']=pd.to_datetime(fct_transactions['transaction_date'])
 start_date='2024-12-01'
 end_date='2024-12-31'
+
+#filter fct_transactions to include only transactions that occurred in December 2024
 filtered_fct_transactions=fct_transactions[(fct_transactions['transaction_date']>=start_date)& (fct_transactions['transaction_date']<=end_date)]
+
+#include a day_of_the_week column to make the problem easier
 filtered_fct_transactions['day_of_the_week']=filtered_fct_transactions['transaction_date'].dt.day_name()
+
+#merge dim_risk_flags and filtered_fct_transactions using left join on the 'transaction_id column
 merged_df=pd.merge(filtered_fct_transactions,dim_risk_flags,how='left',on='transaction_id')
+
+#create another dataframe to include only transactions with High risk levels
 high_risk = merged_df[merged_df['risk_level'] == 'High']
+
+#calculate the size of the list after it has been grouped by 'day_of_the_week'
 counts = high_risk.groupby('day_of_the_week').size()
+
+#assign to most_common_day the value of the day of the week with the most High risk transactions
 most_common_day = counts.idxmax()
+
+
 print(most_common_day, counts[most_common_day])
