bf515bdce62bae54921d871ec8fd8eca42fa7655
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-14 20:41:16 +0100

    Create Question 3.py

---
+++ Day 2/Question 3.py
@@ -0,0 +1,24 @@
+#For the product categories identified in the previous question, what is the percentage difference between their CTR and the overall average CTR for October 2024? This analysis will quantify the performance gap to recommend specific categories for targeted advertising optimization.
+
+import pandas as pd
+#merge fct_ad_performance and dim_product on the column product_id
+new_df = pd.merge(fct_ad_performance,dim_product, on="product_id")
+
+#filter the dataframe to only include  rows where the product_category contains the string 'Electronics'
+new_df = new_df[new_df['product_category'].str.contains("Electronics")]
+
+#convert the recorded_date to datetime format
+new_df['recorded_date']=pd.to_datetime(new_df['recorded_date'] )
+
+
+
+#filter created_date include only those that were recorded in October 2024
+start_date = '2024-10-01'
+end_date = '2024-10-31'
+new_df = new_df[(new_df['recorded_date'] >= start_date) & (new_df ['recorded_date'] <= end_date)]
+
+#create a new column and assign to it the value of the click-through rate (clicks/impressions * 100)
+new_df=new_df.assign(ctr=(new_df["clicks"] / new_df["impressions"])*100)
+
+#group by product_category and calculate the average ctr
+new_df = new_df.groupby('product_category')['ctr'].mean()
