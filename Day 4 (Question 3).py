908bb53967b270e307d73181a101f791c0e033ab
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-15 17:39:57 +0100

    Delete Day 4 (Question 3).py

--- Day 4 (Question 3).py
+++
@@ -1,8 +0,0 @@
-user_engagement_data=user_engagement_data.drop_duplicates()
-cleaned_df=user_engagement_data.fillna(0.0)
-cleaned_df['average_interaction_time']=cleaned_df.groupby('search_results_displayed')['interaction_time'].transform('mean')
-# cleaned_df=cleaned_df.assign(average_user_interaction_time=(cleaned_df['interaction_time']/cleaned_df['search_results_displayed']))
-# cleaned_df=[['search_results_displayed','average_interaction_time']]
-cleaned_df=cleaned_df[['search_results_displayed','average_interaction_time']].drop_duplicates().sort_values(by='average_interaction_time',ascending=False)
-highest_average_interaction_time=cleaned_df.iloc[0]
-print(highest_average_interaction_time)
