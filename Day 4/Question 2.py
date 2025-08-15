3d3f14cf954415d4ac17cfecccca491009fa9b68
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-15 17:45:18 +0100

    Create Question 2.py

---
+++ Day 4/Question 2.py
@@ -0,0 +1,9 @@
+#After dropping duplicates, aggregate the data to find the average user interaction time for each number of search results displayed per page. What are the average interaction times?
+
+
+user_engagement_data=user_engagement_data.drop_duplicates()
+cleaned_df=user_engagement_data.fillna(0.0)
+cleaned_df['average_interaction_time']=cleaned_df.groupby('search_results_displayed')['interaction_time'].transform('mean')
+
+cleaned_df=cleaned_df[['search_results_displayed','average_interaction_time']].drop_duplicates().to_string(index=False)
+print(cleaned_df)
