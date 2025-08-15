1d5034f407e3851daaac783586358aec0f2730da
Jadesola2 <jadesolaojo07@gmail.com>
2025-08-15 19:13:09 +0100

    Update and rename Question 1.py to Day 9/Question 1.py

--- Day 9/Question 1.py
+++ Day 9/Question 1.py
@@ -5,6 +5,6 @@
 # The following tables are loaded as pandas DataFrames with the same names: stories_data
 # Please print your final result or dataframe
 
-#convert the elements
+#convert the elements in the story_date column into datetime objects
 stories_data['story_date']=pd.to_datetime(stories_data['story_date'],errors='coerce')
 print(stories_data)
